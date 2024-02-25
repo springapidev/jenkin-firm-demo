e8c5bc8296c63783fa3ac3457499266a0b38c4e0
Mohammad Rajaul Islam <springapidev@gmail.com>
2024-02-25 22:59:18 +0600

    COmmit 1

---
+++ src/main/java/com/example/jenkinfirmdemo/JenkinFirmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinfirmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinFirmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinFirmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinfirmdemo/JenkinFirmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinfirmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinFirmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
